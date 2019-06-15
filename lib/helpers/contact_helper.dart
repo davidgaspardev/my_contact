a3f88294710068e29a21c89a4c33a73afb89fae2
David Gaspar <davidgaspar.dev@gmail.com>
2019-06-14 22:40:29 -0300

    Getter/Setter methods in contact

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -18,7 +18,7 @@ class ContactHelper {
 
   Database _db;
 
-  Future<Database> get dg async {
+  Future<Database> get db async {
     if(_db != null) {
       return _db;
     } else {
@@ -34,9 +34,8 @@ class ContactHelper {
     // Path with file (path complete)
     final path = join(databasePath, "contacts.db");
 
-
-
-    Database db = await openDatabase(path, version: 1, onCreate: (Database db, int newVersion) async {
+    // Opening database
+    Database newDb = await openDatabase(path, version: 1, onCreate: (Database db, int newVersion) async {
 
       await db.execute(
         // Code SQL
@@ -50,7 +49,37 @@ class ContactHelper {
 
     });
 
-    return db;
+    return newDb;
+
+  }
+
+  // Methods setter and getter (Contact)
+  Future<Contact> setContact(Contact contact) async {
+    // Get database
+    Database dbContact = await db;
+
+    // Insert contact in database's table
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+
+    return contact;
+  }
+
+  Future<Contact> getContact(int id) async {
+    // Get database
+    Database dbContact = await db;
+
+    // Get data from database (filter id)
+    List<Map> contact = await dbContact.query(contactTable,
+        columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
+        where: "$idColumn == ?",
+        whereArgs: [id]
+    );
+
+    if(contact.length > 0) {
+      return Contact.fromMap(contact.first);
+    }else {
+      return null;
+    }
 
   }
 
