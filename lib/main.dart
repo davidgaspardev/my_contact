ddbbf49ca9c3b8f0b5b1b61a7e7f28abad9056bd
David Gaspar <davidgaspar.dev@gmail.com>
2019-06-17 20:00:46 -0300

    Add app bar with Scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,40 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: MainActivity.appName,
+      home: MainActivity(),
+    );
   }
+}
+
+class MainActivity extends StatefulWidget {
+
+  static final String appName = "My Contacts";
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MainActivity();
+  }
+
+}
+
+class _MainActivity extends State<MainActivity> {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(MainActivity.appName),
+        backgroundColor: Colors.red,
+        centerTitle: true
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+
+
+
 }
